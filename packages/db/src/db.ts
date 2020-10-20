8c92b8361eff025b9a017ef1d32a0d90f7eca6d0
g. nicholas d'andrea <gnidan@trufflesuite.com>
2020-10-20 01:01:37 -0400

    Move loader methods to separate class
    - To be used as `db.loaders.loadProject()`, etc.
    - Require IdObject<Project> for all methods
    - Favor IdObjects as arguments, compensate by querying for `name`

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,15 +1,8 @@
 import { GraphQLSchema, DocumentNode, parse, execute } from "graphql";
-import { generateCompileLoad } from "@truffle/db/loaders/commands";
-import { WorkspaceRequest, WorkspaceResponse } from "@truffle/db/loaders/types";
-import { WorkflowCompileResult } from "@truffle/compile-common";
+import { Loaders } from "./loaders";
 import { schema } from "./schema";
 import { connect } from "./connect";
 import { Databases } from "./definitions";
-import {
-  generateInitializeLoad,
-  generateNamesLoad
-} from "@truffle/db/loaders/commands";
-import { toIdObject, NamedResource } from "@truffle/db/meta";
 
 interface IConfig {
   contracts_build_directory: string;
@@ -35,17 +28,18 @@ interface ITruffleDB {
   query: (query: DocumentNode | string, variables: any) => Promise<any>;
 }
 
-type LoaderOptions = {
-  names: boolean;
-};
-
 export class TruffleDB {
-  schema: GraphQLSchema;
-  context: IContext;
+  public schema: GraphQLSchema;
+  public loaders: Loaders;
+
+  private context: IContext;
 
   constructor(config: IConfig) {
     this.context = this.createContext(config);
     this.schema = schema;
+    this.loaders = new Loaders({
+      db: this
+    });
   }
 
   async query(query: DocumentNode | string, variables: any = {}): Promise<any> {
@@ -53,69 +47,6 @@ export class TruffleDB {
       typeof query !== "string" ? query : parse(query);
 
     return await execute(this.schema, document, null, this.context, variables);
-  }
-
-  async getWorkspaceResponse(generatorRequest: WorkspaceRequest) {
-    const { request, variables }: WorkspaceRequest = generatorRequest;
-
-    const response: WorkspaceResponse = await this.query(request, variables);
-
-    return response;
-  }
-
-  private async runLoader<
-    Request extends WorkspaceRequest,
-    Response extends WorkspaceResponse,
-    Args extends unknown[],
-    Return
-  >(
-    loader: (...args: Args) => Generator<Request, Return, Response>,
-    ...args: Args
-  ): Promise<Return> {
-    const saga = loader(...args);
-    let current = saga.next();
-
-    while (!current.done) {
-      const { request, variables } = current.value as Request;
-
-      const response: Response = await this.query(request, variables);
-
-      current = saga.next(response);
-    }
-
-    return current.value;
-  }
-
-  async loadNames(project: DataModel.IProject, resources: NamedResource[]) {
-    return await this.runLoader(
-      generateNamesLoad,
-      toIdObject(project),
-      resources
-    );
-  }
-
-  async loadProject(): Promise<DataModel.IProject> {
-    return await this.runLoader(generateInitializeLoad, {
-      directory: this.context.workingDirectory
-    });
-  }
-
-  async loadCompilations(
-    result: WorkflowCompileResult,
-    options: LoaderOptions
-  ) {
-    const project = await this.loadProject();
-
-    const { compilations, contracts } = await this.runLoader(
-      generateCompileLoad,
-      result
-    );
-
-    if (options.names === true) {
-      await this.loadNames(project, contracts);
-    }
-
-    return { compilations, contracts };
   }
 
   createContext(config: IConfig): IContext {
